67c535d452d277afc1ecf7b292d12bb7d794ef19
pehtoorismies <pehtoorismies@gmail.com>
2018-12-10 18:35:00 +0200

    Updates

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const config = {};
+const config = {
+  AUTH0: {
+    DOMAIN: process.env.AUTH0_DOMAIN,
+    CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+  },
+  PRISMA_ENDPOINT: 'http://localhost:4466',
+};
 
 export default config;
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,9 +1,72 @@
 // @flow
+import { ManagementClient, AuthenticationClient } from 'auth0';
+import config from './config';
+
+const { DOMAIN, CLIENT_ID, CLIENT_SECRET } = config.AUTH0;
+
+const auth0 = new AuthenticationClient({
+  domain: DOMAIN,
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+});
+
+const createAuthZeroUser = async (email, password, nick, name) => {
+  // valid user => create to Auth0
+  const client = await auth0.clientCredentialsGrant({
+    audience: `https://${DOMAIN}/api/v2/`,
+    scope: 'create:users',
+  });
+  const management = new ManagementClient({
+    token: client.access_token,
+    domain: DOMAIN,
+  });
+  const authZeroUser = await management.createUser({
+    connection: 'Username-Password-Authentication',
+    email,
+    password,
+    username: nick,
+    user_metadata: {
+      name,
+    },
+  });
+  return authZeroUser;
+};
+
+type CreateUserInput = {
+  email: String,
+  password: String,
+  nick: String,
+  name: ?String,
+};
+
 const resolvers = {
   Query: {
     users: async (_: void, __: void, { prisma }) => {
       const allUsers = await prisma.users();
       return allUsers;
+    },
+  },
+  Mutation: {
+    registerUser: async (_: void, args: CreateUserInput, { prisma }) => {
+      try {
+        const { email, password, nick, name } = args;
+        const { user_id: providerID } = await createAuthZeroUser(
+          email,
+          password,
+          nick,
+          name
+        );
+        const user = await prisma.createUser({
+          providerID,
+          email,
+          name,
+          nick,
+        });
+
+        return user;
+      } catch (error) {
+        return error;
+      }
     },
   },
 };
